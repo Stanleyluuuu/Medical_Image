e07c6fbe2ad55233a25ec7455fa1816bd120c5b4
stanleyluuuu <stanley860920@gmail.com>
2021-03-29 04:02:57 +0800

    finish part 2

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -6,61 +6,75 @@ import os
 import pdb
 
 
-# def plot_3d(image, threshold=-300): 
-#     # p = image.transpose(2,1,0)
-#     p = image
-#     verts, faces, normals, values = measure.marching_cubes_lewiner(p, threshold)
-#     fig = plt.figure(figsize=(10, 10))
-#     ax = fig.add_subplot(111, projection='3d')
-#     mesh = Poly3DCollection(verts[faces], alpha=0.1)
-#     face_color = [0.5, 0.5, 1]
-#     mesh.set_facecolor(face_color)
-#     ax.add_collection3d(mesh)
-#     ax.set_xlim(0, p.shape[0])
-#     ax.set_ylim(0, p.shape[1])
-#     ax.set_zlim(0, p.shape[2])
-
-#     plt.show()
-
-
-# datadir = './CT_chest_scans'
-
-# folders = os.listdir(datadir)
-# for folder in folders:
-#     imagedir = os.path.join(datadir, folder)
-#     images = os.listdir(imagedir)
-#     data = {}
-#     for image in images:
-#         image_path = os.path.join(imagedir, image)
-#         pic = pydicom.dcmread(image_path)
-#         data[pic.InstanceNumber] = pic.pixel_array
-#     volume = []
-#     for ins in sorted(data):
-#         volume.append(data[ins])
-#         # pdb.set_trace()
-    
-#     plot_3d(np.array(volume))
-# # pdb.set_trace()
-
 def get_hu(pic):
-    pixel_value = pic.pixel_array
+    pixel_value = np.clip(pic.pixel_array, 0, 255)
     rescale_intercept = pic[0x0028, 0x1052].value
     rescale_slope = pic[0x0028, 0x1053].value
     hounsfield_units = (rescale_slope * pixel_value) + rescale_intercept
+    nor_hu = normalize_hu(hounsfield_units)
 
-    return hounsfield_units
+    return nor_hu
 
+def normalize_hu(hounsfield_units):
+    nor_hu = np.array([(x+1024) / 4096 for x in hounsfield_units])
+
+    return np.float32(nor_hu)
 
 datadir = './CT_chest_scans'
-ct_hounsfield = []
+
 folders = os.listdir(datadir)
 for folder in folders:
     imagedir = os.path.join(datadir, folder)
     images = os.listdir(imagedir)
+    data = {}
     for image in images:
         image_path = os.path.join(imagedir, image)
         pic = pydicom.dcmread(image_path)
         hounsfield_units = get_hu(pic)
-        ct_hounsfield.append(hounsfield_units)
+        pdb.set_trace()
+        data[pic.InstanceNumber] = hounsfield_units
+    volume = []
+    for ins in sorted(data):
+        volume.append(data[ins])
+    fig=plt.figure(figsize=(8, 8))
+    columns = 5
+    rows = 5
+
+    for i in range(5): ## load the data from 9 classes
+        # a = list(np.where(labels == i))[0]
+        for j in range(5): ## each 5 pictures for every 9 classes
+            fig.add_subplot(rows, columns, i * 5 + j + 1)
+            plt.imshow(volume[i * 5 + j])
+            plt.axis('off')
+    plt.savefig('result1.png', cmap=plt.cm.bone)
+    pdb.set_trace()
+        
+pdb.set_trace()
 
 
+# def get_hu(pic):
+#     pixel_value = np.clip(pic.pixel_array, 0, 255)
+#     rescale_intercept = pic[0x0028, 0x1052].value
+#     rescale_slope = pic[0x0028, 0x1053].value
+#     hounsfield_units = (rescale_slope * pixel_value) + rescale_intercept
+#     nor_hu = normalize_hu(hounsfield_units)
+
+#     return nor_hu
+
+# def normalize_hu(hounsfield_units):
+#     nor_hu = np.array([(x+1024) / 4096 for x in hounsfield_units])
+
+#     return nor_hu
+
+# datadir = './CT_chest_scans'
+# ct_hounsfield = []
+# folders = os.listdir(datadir)
+# for folder in folders:
+#     imagedir = os.path.join(datadir, folder)
+#     images = os.listdir(imagedir)
+#     for image in images:
+#         image_path = os.path.join(imagedir, image)
+#         pic = pydicom.dcmread(image_path)
+#         hounsfield_units = get_hu(pic)
+#         ct_hounsfield.append(hounsfield_units)
+# pdb.set_trace()
